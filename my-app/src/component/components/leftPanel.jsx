13e97efca39e4b7240d4f777af4e82f1816942de
Takemyeye <mellovan2005@gmail.com>
2024-06-23 05:57:53 +0200

    /

--- my-app/src/component/components/leftPanel.jsx
+++ my-app/src/component/components/leftPanel.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useMemo, useEffect} from "react";
+import React, { useContext, useMemo, useEffect } from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faDownload } from '@fortawesome/free-solid-svg-icons';
 import { Link } from "react-router-dom";
@@ -11,8 +11,8 @@ const translations = {
     cpp: 'Cisco Certification',
     git: 'All my Project',
     download: 'Download Resume',
-    description:'Description',
-    skills:'My Skills'
+    description: 'Description',
+    skills: 'My Skills'
   },
   ru: {
     react: 'Интернет-магазин Soul Swap',
@@ -35,8 +35,7 @@ const translations = {
 };
 
 const LeftPanel = () => {
-  const { isActive,language } = useContext(ActiveContext); 
-  
+  const { isActive, language } = useContext(ActiveContext); 
 
   useEffect(() => {
     localStorage.setItem('language', language);
@@ -44,35 +43,49 @@ const LeftPanel = () => {
 
   const headerPhrase = useMemo(() => translations[language], [language]);
 
-  return(
+  const getResumeFileName = () => {
+    switch (language) {
+      case 'eng':
+        return 'resume.eng.pdf';
+      case 'ru':
+        return 'resume.ru.pdf';
+      case 'it':
+        return 'resume.it.pdf';
+      default:
+        return 'resume.eng.pdf'; 
+    }
+  };
+
+  return (
     <div className="leftPanel">
       <div className="conteinerLeft">
         <div className="imgCircle">
           <div className="img"></div>
         </div>
-          <a href="/resume.pdf" download="Oleh_Bratok_Resume.pdf">
-            <button className="download">{headerPhrase.download} <FontAwesomeIcon icon={faDownload} /></button>
-          </a>      
-        </div>
+        <a href={getResumeFileName()} download={`Oleh_Bratok_Resume.${language}.pdf`}>
+          <button className="download">{headerPhrase.download} <FontAwesomeIcon icon={faDownload} /></button>
+        </a>
+      </div>
       <div className="mobile">
         <div className={`conteinerRight ${isActive ? 'active' : ''}`}>        
           <div className="text-skills">
-            <h4>{headerPhrase.Description}</h4>
-              <div className="descriotion"><Link to={"https://github.com/Takemyeye/SS"}>{headerPhrase.react}</Link></div>
-              <div className="descriotion"><Link to={"https://github.com/Takemyeye/SS"}>{headerPhrase.scss}</Link></div>
-              <div className="descriotion"><Link to={"https://www.netacad.com/"}>{headerPhrase.cpp}</Link></div>
-              <div className="descriotion"><Link to={"https://github.com/Takemyeye"}>{headerPhrase.git}</Link></div>
+            <h4>{headerPhrase.description}</h4>
+            <div className="descriotion"><Link to={"https://github.com/Takemyeye/SS"}>{headerPhrase.react}</Link></div>
+            <div className="descriotion"><Link to={"https://github.com/Takemyeye/SS"}>{headerPhrase.scss}</Link></div>
+            <div className="descriotion"><Link to={"https://www.netacad.com/"}>{headerPhrase.cpp}</Link></div>
+            <div className="descriotion"><Link to={"https://github.com/Takemyeye"}>{headerPhrase.git}</Link></div>
           </div>
-            <div className="skills">
-              <h4>{headerPhrase.skills}</h4>
-                <div className="ui"></div>
-                <div className="ui"></div>
-                <div className="ui"></div>
-                <div className="ui"></div>
-            </div>
+          <div className="skills">
+            <h4>{headerPhrase.skills}</h4>
+            <div className="ui"></div>
+            <div className="ui"></div>
+            <div className="ui"></div>
+            <div className="ui"></div>
           </div>
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
+
 export default LeftPanel;
